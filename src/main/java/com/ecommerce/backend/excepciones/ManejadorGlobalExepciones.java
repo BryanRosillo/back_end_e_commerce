0aecc275380fac7114f53e986f6dad1af0fc05dd
BryanRosillo <108194629+BryanRosillo@users.noreply.github.com>
2025-01-12 18:55:36 -0500

    feat: funcionalidad agregada de manejo de excepciones

---
+++ src/main/java/com/ecommerce/backend/excepciones/ManejadorGlobalExepciones.java
@@ -0,0 +1,26 @@
+package com.ecommerce.backend.excepciones;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ManejadorGlobalExepciones {
+	
+	private final String NOMBRE_VARIABLE_ERROR = "error";
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> manejarExcepcionesGenerales(Exception e){
+		log.error("Hubo un problema en el servidor: {}", e.getMessage(), e);
+		Map<String, String> error = new HashMap<>();
+		error.put(NOMBRE_VARIABLE_ERROR, "Lo siento, hubo un problema. Informe al desarrollador.");
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+	}
+
+}
